2b16c6ee6948285959a352a56bc3759e38fe6103
Jeremy_Hester <west.fl.itguy@mail.com>
2023-10-04 13:24:49 -0400

    fix order id

--- roi/blueprints/api/routes.py
+++ roi/blueprints/api/routes.py
@@ -76,7 +76,7 @@ def create_order(cust_id):
     db.session.add(order)
     for product in customer_order:
         print(f"cust_id in for loop {cust_id}")
-        query = f'INSERT INTO productOrder (prodorder_id, prod_id, quantity, price, order_id, cust_id) VALUES ({product["prod_id"]}, {product["quantity"]}, {product["price"]},{order.order_id},{cust_id}) '
+        query = f'INSERT INTO productOrder (prodorder_id, prod_id, quantity, price, order_id, cust_id) VALUES (\'{product["prod_id"]}\', {product["quantity"]}, {product["price"]},{order.order_id},{cust_id}) '
         prodorder = db.session.execute(text(query))
         db.session.add(prodorder)
 
