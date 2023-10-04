b2a4207b1c5288c2f797d964f3a8d7f2307b2e1d
Jeremy_Hester <west.fl.itguy@mail.com>
2023-10-04 13:20:09 -0400

    fix text import

--- roi/blueprints/api/routes.py
+++ roi/blueprints/api/routes.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint, request, jsonify 
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from flask_sqlalchemy import text
+from sqlalchemy import text
 
 from models import Users, Product, ProdOrder, Order, Customer, db, products_schema, product_schema
 
